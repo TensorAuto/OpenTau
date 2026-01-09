75e5d10d8db30fa95bceec0abf99f4fc4ad73744
William Yue <37453238+WilliamYue37@users.noreply.github.com>
2026-01-08 16:08:00 -0800

    Fix issue with accelerate looking up the metadata of the transformers package (#56)

--- src/opentau/__init__.py
+++ src/opentau/__init__.py
@@ -56,6 +56,7 @@ When implementing a new policy class (e.g., `DiffusionPolicy`), follow these ste
 import itertools
 
 from opentau.__version__ import __version__  # noqa: F401
+from opentau.utils import transformers_patch  # noqa: F401
 
 # TODO(rcadene): Improve policies and envs. As of now, an item in `available_policies`
 # refers to a yaml file AND a modeling name. Same for `available_envs` which refers to
